d26226cbf1aa0ec6054bd43595ac04dbcd4057d9
Ankit kumar duvedi <ankitduvedi.03@gmail.com>
2023-05-05 03:13:39 +0530

    third

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.blue,
         body: Center(
           child: Text('Hello world'),
         ),
