3ad38d1b1798cbf27b62794e04971bbb41e62669
Ankit kumar duvedi <ankitduvedi.03@gmail.com>
2023-05-06 04:46:59 +0530

    images added

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,26 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:app/styled_text.dart';
+
 class GradientContainer extends StatelessWidget {
   const GradientContainer({super.key});
   @override
-  Widget build(context){
+  Widget build(context) {
     return Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Colors.purpleAccent,
-                Colors.blueAccent,
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-          
-            ),
-          ),
-          child: const Center(
-            child:StyledText('Hey king'),
-
-          ),
-        );
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Colors.purpleAccent,
+            Colors.blueAccent,
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: Center(
+        child: Image.asset(
+          'assets/images/dice-2.png',
+          width: 200,
+        ),
+      ),
+    );
   }
-
 }
